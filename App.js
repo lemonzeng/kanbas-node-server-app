b57fdf232cf5fcdc3493c73bc13f56dabd4b26bc
lemonzeng <yumengzeng.zeng@gmail.com>
2024-07-22 22:05:23 +0800

    Finish Lab6

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
+import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import cors from "cors";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import UserRoutes from "./Kanbas/Users/routes.js";
 
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,6 +18,7 @@ app.use(express.json());
 AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
+UserRoutes(app);
 
 Lab5(app);
 Hello(app);
